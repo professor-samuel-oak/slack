d2e4705d5b043e138b3e131fd8aa49746d22fdfb
Dennis Fokker <dennis.fokker@live.nl>
2018-01-31 22:33:15 +0100

    Created enums for (pretty much) static names
    Pokemon and move names not added since they can potentially be extended (with future generations).

---
+++ source/Enums/IVName.ts
@@ -0,0 +1,9 @@
+enum IVName {
+    HP,
+    ATTACK,
+    DEFENSE,
+    SPEED,
+    SPECIAL
+}
+
+export default IVName;
---
+++ source/Enums/StatName.ts
@@ -0,0 +1,12 @@
+enum StatName {
+    HP,
+    ATTACK,
+    DEFENSE,
+    SPEED,
+    SPECIAL_ATTACK,
+    SPECIAL_DEFENSE,
+    EVASION,
+    ACCURACY
+}
+
+export default StatName;
