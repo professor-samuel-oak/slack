39f697740e8c4ceef5443e2339875b716d2b290f
Dennis Fokker <dennis.fokker@live.nl>
2018-02-01 16:25:32 +0100

    Added documentation

--- source/Helpers/IV.ts
+++ source/Helpers/IV.ts
@@ -3,6 +3,11 @@ import StatName from "Enums/StatName";
 
 export default class IVHelper {
 
+    /**
+     * Converts a statistic name to it's matching IV name.
+     * @param statName Stat name to convert.
+     * @returns Converted IV name.
+     */
     public static statNameToIVName (statName: StatName): IVName {
         switch (statName) {
             case StatName.ATTACK: return IVName.ATTACK;
--- source/Helpers/Stat.ts
+++ source/Helpers/Stat.ts
@@ -3,6 +3,14 @@ import Stat from "Models/Pokemon/Stat";
 
 export default class StatHelper {
 
+    /**
+     * Calculates the statistic's actual value.
+     * @param stat Statistic to calculate.
+     * @param stage Stage of statistic.
+     * @param level Level of pokemon.
+     * @param IV IV value of statistic.
+     * @returns Statistic's actual value.
+     */
     public static calculateStat (stat: Stat, stage: number, level: number, IV: number): number {
         let value = Math.floor((stat.value + IV) * 2 * level / 100);
         value += stat.name === StatName.HP ? level + 10 : 5;
@@ -14,6 +22,12 @@ export default class StatHelper {
         return value;
     }
 
+    /**
+     * Get the statistic's stage modifier.
+     * @param statName Statistic's name.
+     * @param stage Statistic's stage.
+     * @returns Statistic's stage modifier.
+     */
     public static getStageModifier (statName: StatName, stage: number): number {
         let base = 2;
         if (statName === StatName.ACCURACY || statName === StatName.EVASION) {
